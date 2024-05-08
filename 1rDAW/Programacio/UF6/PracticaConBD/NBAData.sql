b2b00ae3978d42280e598fd7e60bbccf470223b6
Xavi <ravirubio2621@gmail.com>
2024-05-08 13:28:29 +0200

    Añadir base de datos

---
+++ 1rDAW/Programacio/UF6/PracticaConBD/NBAData.sql
@@ -0,0 +1,58 @@
+/* Eliminem la base de dades si existeix */
+DROP DATABASE IF EXISTS NBAData;
+
+/* Crear la base de dades */
+CREATE DATABASE NBAData;
+
+USE NBAData;
+
+CREATE TABLE players
+(
+	id VARCHAR(10) NOT NULL,
+    full_name VARCHAR(65) GENERATED ALWAYS AS(concat(first_name," ", last_name)),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    is_active BOOLEAN,
+    CONSTRAINT pk_players PRIMARY KEY(id)
+);
+
+CREATE TABLE teams
+(
+	id VARCHAR(10) NOT NULL,
+    full_name VARCHAR(65) NOT NULL,
+    abbreviation VARCHAR(10) NOT NULL,
+    nickname VARCHAR(30),
+	city VARCHAR(30),
+    state VARCHAR(30),
+    is_active BOOLEAN,
+    CONSTRAINT pk_teams PRIMARY KEY(id)
+);
+
+CREATE TABLE matchupsrollup
+(
+	LeagueID VARCHAR(10) NOT NULL,
+    Season DECIMAL NOT NULL,
+    SeasonType VARCHAR(30) NOT NULL,
+	OffTeamID VARCHAR(30),
+    OffPlayerID VARCHAR(30),
+    DefTeamID VARCHAR(30),
+	DefPlayerID VARCHAR(30)
+);
+
+INSERT INTO players (id, first_name, last_name, is_active) VALUE
+	(1,"Mohamed", "Ali", true),
+    (2, "Pedro", "Sanchez", false),
+    (3, "Michael", "Jordan", true);
+
+INSERT INTO teams (id, full_name, abbreviation, nickname, city, state, is_active) VALUE 
+	(1,"Los Juanes", "LJ", "Siuuuu", "Barcelona", "BarcelonaState", true),
+    (2,"Los Pepes", "LP", "Nouuuuu", "Madrid", "MadridState", true),
+    (3,"Los Mindundis", "LM", "NoHaceNada", "Valladolid", "ValladolidState", true);
+
+INSERT INTO matchupsrollup (LeagueId, Season, SeasonType, OffTeamID, OffPlayerID, DefTeamID, DefPlayerID) VALUE 
+	(1, 1, "Siu", 1, 1, 2,2),
+    (2, 2, "Siu", 2, 2, 3,3),
+    (3, 3, "Siu", 1, 1, 3,3	);
+
+
+SELECT * FROM teams;
