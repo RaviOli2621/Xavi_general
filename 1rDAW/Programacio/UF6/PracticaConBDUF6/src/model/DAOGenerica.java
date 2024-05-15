9e09be8ac6db56f856bb2e8207f3e77374029cd9
Xavi <ravirubio2621@gmail.com>
2024-05-15 11:05:32 +0200

    Practica prog UF6: DAO general

--- 1rDAW/Programacio/UF6/PracticaConBDUF6/src/model/DAOGenerica.java
+++ 1rDAW/Programacio/UF6/PracticaConBDUF6/src/model/DAOGenerica.java
@@ -1,4 +1,16 @@
 package model;
 
-public interface DAOGenerica {
+import java.util.List;
+
+public interface DAOGenerica<T>
+{
+    // CRUD
+    boolean create(T t);
+    boolean read(T t);
+    boolean update(T t);
+    boolean delete(T t);
+    // ALTRES
+    boolean exists(T t);
+    int count();
+    List<T> all();
 }
