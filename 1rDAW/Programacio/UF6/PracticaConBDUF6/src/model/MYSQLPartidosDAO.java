e1dfd0a92924c2e413a1c9aa5dd2f84d43705050
David Vaquera <d.vaquera@sapalimera.cat>
2024-05-17 12:17:27 +0200

    Practica UF6 : PartidosDAO acabado

--- 1rDAW/Programacio/UF6/PracticaConBDUF6/src/model/MYSQLPartidosDAO.java
+++ 1rDAW/Programacio/UF6/PracticaConBDUF6/src/model/MYSQLPartidosDAO.java
@@ -1,32 +1,109 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MYSQLPartidosDAO implements DAOGenerica{
+public class MYSQLPartidosDAO implements DAOGenerica<Partidos>{
+    Connection con;
+
+    public MYSQLPartidosDAO(Connection con) {
+        this.con = con;
+    }
+
+    // CRUD, implementarlo como objeto
+
     @Override
-    public boolean create(Object o, Connection con) {
+    public boolean create(Partidos p, Connection con) {
+        PreparedStatement sta;
+        try {
+            sta = con.prepareStatement("INSERT INTO partits (partit_id, equip_id, resultat, data_partit, matx) " +
+                    "VALUES (?,?,?,?,?)");
+            sta.setInt(1,p.getPartit_id());
+            sta.setInt(2,p.getEquip_id());
+            sta.setString(4,p.getResultat());
+            sta.setDate(3,p.getData_partit());
+            sta.setString(8,p.getMatx());
+            sta.executeUpdate();
+            return true;
+        }catch (SQLException s)
+        {
+            System.out.println("Error al crear: " + s.getMessage());
+        }
         return false;
     }
 
     @Override
-    public boolean read(Object o, Connection con) {
+    public boolean read(Partidos p, Connection con) {
+        Partidos par = read(p.getPartit_id(),con);
+        if (par == null) return false;
+        p.setEquip_id(par.getEquip_id());
+        p.setResultat(par.getResultat());
+        p.setData_partit(par.getData_partit());
+        p.setMatx(par.getMatx());
+        return true;
+    }
+    private static Partidos read(int partit_id, Connection con) {
+        PreparedStatement sta;
+        ResultSet rs;
+        try {
+            sta = con.prepareStatement("SELECT * FROM partits WHERE partit_id = ?");
+            sta.setInt(1,partit_id);
+            rs = sta.executeQuery();
+            while (rs.next())
+            {
+                if (rs.getString("partit_id").isEmpty()) return null;
+                return new Partidos(partit_id, rs.getInt("equip_id"),rs.getString("resultat"),rs.getDate("data_partit")
+                        ,rs.getString("matx"));
+            }
+        }catch (SQLException s)
+        {
+            System.out.println("Error al hacer select");
+            return null;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(Partidos p, Connection con) {
+        PreparedStatement sta;
+        try {
+            sta = con.prepareStatement("UPDATE jugadors SET partit_id =?,equip_id =?,resultat =?,data_partit =?,matx =?");
+            sta.setInt(1,p.getPartit_id());
+            sta.setInt(2,p.getEquip_id());
+            sta.setString(4,p.getResultat());
+            sta.setDate(3,p.getData_partit());
+            sta.setString(8,p.getMatx());
+            sta.executeUpdate();
+            return true;
+        }catch (SQLException s)
+        {
+            System.out.println("Error al fer update");
+        }
         return false;
     }
 
     @Override
-    public boolean update(Object o, Connection con) {
+    public boolean delete(Partidos p, Connection con) {
+        PreparedStatement sta;
+        try {
+            sta = con.prepareStatement("DELETE FROM partits WHERE partit_id =? ");
+            sta.setInt(1,p.getPartit_id());
+            sta.executeUpdate();
+            return true;
+        }catch (SQLException s)
+        {
+            System.out.println("Error al borrar: " + s.getMessage());
+        }
         return false;
     }
 
     @Override
-    public boolean delete(Object o, Connection con) {
-        return false;
-    }
-
-    @Override
-    public boolean exists(Object o, Connection con) {
-        return false;
+    public boolean exists(Partidos partidos, Connection con) {
+        return true;
     }
 
     @Override
@@ -35,7 +112,9 @@ public class MYSQLPartidosDAO implements DAOGenerica{
     }
 
     @Override
-    public List all() {
-        return null;
+    public List<Partidos> all() {
+        return new ArrayList<>();
     }
+
+
 }
--- 1rDAW/Programacio/UF6/PracticaConBDUF6/src/model/Partidos.java
+++ 1rDAW/Programacio/UF6/PracticaConBDUF6/src/model/Partidos.java
@@ -2,12 +2,12 @@ package model;
 import java.util.Date;
 public class Partidos {
     int partit_id, equip_id;
-    char resultat;
+    String resultat;
     Date data_partit;
     String matx;
 
     //constructors
-    public Partidos(int partit_id, int equip_id, char resultat, Date data_partit, String matx) {
+    public Partidos(int partit_id, int equip_id, String resultat, Date data_partit, String matx) {
         this.partit_id = partit_id;
         this.equip_id = equip_id;
         this.resultat = resultat;
@@ -17,14 +17,14 @@ public class Partidos {
     //getters
     public int getPartit_id() {return partit_id;}
     public int getEquip_id() {return equip_id;}
-    public char getResultat() {return resultat;}
-    public Date getData_partit() {return data_partit;}
+    public String getResultat() {return resultat;}
+    public java.sql.Date getData_partit() {return (java.sql.Date) data_partit;}
     public String getMatx() {return matx;}
 
     //setters
     public void setPartit_id(int partit_id) {this.partit_id = partit_id;}
     public void setEquip_id(int equip_id) {this.equip_id = equip_id;}
-    public void setResultat(char resultat) {this.resultat = resultat;}
+    public void setResultat(String resultat) {this.resultat = resultat;}
     public void setData_partit(Date data_partit) {this.data_partit = data_partit;}
     public void setMatx(String matx) {this.matx = matx;}
 }
